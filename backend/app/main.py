d88544b8e7cc8336212defc86649f6f40331bcee
Eatate080 <eitomarotatu_110@outlook.jp>
2026-07-27 19:03:11 +0000

    register/login-API作成

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,22 @@
 import os
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel  # DBができれば削除
+import Schemas, Crud
+from Crud import EmailAlreadyExistsError
+from pydantic import BaseModel  # Schemas.pyで実装待ち
 from sqlalchemy.orm import Session
 
 from app.db import Base, engine, get_db
+
+
+# 消したい部分
+class UserLogin(BaseModel):  # フロント側とは乖離しているemailがid
+    email: Emailstr
+    password: str
+
+
+# ここまで
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,30 +33,39 @@ app.add_middleware(
 )
 
 
-class RegisterInput(BaseModel):
-    id: int | str  # コレが何か分からない
-    username: str
-    password: str
-
-
-class LoginInput(BaseModel):
-    id: int | str  # RegisterInputと同様
-    password: str
-
-
 @app.get("/")
 def root():
     return {"message": "Hello Hackathon"}
 
 
-@app.post("/register")
-def register(data: RegisterInput, db: Session = Depends(get_db)):
-    # ユーザーをDBに保存する
-    pass
+# ユーザー登録
+@app.post("/register", response_model=Schemas.UserResponse)
+def register(user: Schemas.UserCreate, db: Session = Depends(get_db)):
+    try:
+        new_user = Crud.create_user(db, user)
+    except EmailAlreadyExistsError:
+        raise HTTPException(
+            status_code=400, detail="このメールアドレスは既に登録されています"
+        )
+
+    return new_user
 
 
-@app.post("/login")
-def login(data: LoginInput, db: Session = Depends(get_db)):
-    # idとpasswordが一致するユーザーがいるか確認する
-    # 一致すれば {"message": "ログイン成功"} のような簡易的な返答がいいかも
-    pass
+# ユーザーログイン
+@app.post("/login", response_model=Schemas.UserResponse)
+def login(
+    user: Schemas.UserLogin, db: Session = Depends(get_db)
+):  # UserLoginはまだない
+    login_user = Crud.get_user_by_email(db, user.email)
+
+    if login_user is None:
+        raise HTTPException(
+            status_code=400, detail="emailまたはpasswordが間違っています"
+        )
+
+    if not Crud.verify_password(user.password, login_user.password_hash):
+        raise HTTPException(
+            status_code=400, detail="emailまたはpasswordが間違っています"
+        )
+
+    return login_user
