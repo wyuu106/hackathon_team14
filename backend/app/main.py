b9b57c6e5fc76c804ed126431b19ac8fa45814f8
砂山　光 <hikarusunayama@sunayamahikarinoMacBook-Air.local>
2026-08-01 15:56:14 +0900

    main.py 切り戻し切り戻し”

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel  # Schemas.pyで実装待ち
@@ -12,6 +14,47 @@ from app.Auth import create_access_token, get_current_user
 
 # Schemas.pyに追記して欲しい？Discord見て判断
 # class UserLogin(BaseModel):
+#     user_id: str  # ユーザー識別子
+#     password: str
+# あと、DB系のファイルインポートにて、相対パスのfrom app入れる。
+
+# Crud.pyに追記(変更)
+# from app import Models, Schemas
+
+# Models.pyに追記(変更)
+# from app.db import Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = os.getenv("ALLOW_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello Hackathon"}
+
+
+# ユーザー登録
+@app.post("/register", response_model=Schemas.UserResponse)
+def register(user: Schemas.UserCreate, db: Session = Depends(get_db)):
+    try:
+        new_user = Crud.create_user(db, user)
+    except UsernameAlreadyExistsError:
+        raise HTTPException(
+            status_code=400, detail="このユーザーIDは既に使われています"
+        )
+
+    return new_user
 
 
 # ユーザーログイン
@@ -22,6 +65,11 @@ def login(
     login_user = Crud.get_user_by_user_id(db, user.user_id)
 
     if login_user is None:
+        raise HTTPException(
+            status_code=400, detail="usernameまたはpasswordが間違っています"
+        )
+
+    if not Crud.verify_password(user.password, login_user.password_hash):
         raise HTTPException(
             status_code=400, detail="usernameまたはpasswordが間違っています"
         )
